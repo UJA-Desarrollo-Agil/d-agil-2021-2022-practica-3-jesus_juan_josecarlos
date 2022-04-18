ba6aa68d46c635332cff69632be021685c479216
Jesus <jpc00068@red.ujaen.es>
2022-04-18 21:35:21 +0200

    Creación situación Tiro libre

--- games/media/games/tutorial/tutorial.game.en.js
+++ games/media/games/tutorial/tutorial.game.en.js
@@ -172,6 +172,19 @@ undum.game.situations = {
         
     ),
 	
+	tiros_libres: new undum.SimpleSituation(
+        "<h1>Trabajas en una empresa</h1>\
+		<img src='media/games/tutorial/tiro_libre.jpg' class='float_right'>\
+		<p>...</p>",
+		{
+			enter: function(character, system, to) {
+                system.setQuality("ahorros", character.qualities.ahorros+10000);
+				system.setQuality("experiencia", character.qualities.experiencia+50);
+            }
+		}
+        
+    ),
+	
     todo: new undum.SimpleSituation(
         "<p>Two things can happen in a situation. The character either\
         <a href='links'>leaves</a> the situation and enters another one, or\
